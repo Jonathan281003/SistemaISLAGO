e5a1c981cbd486de600efc6bc1010f2739dfd143
Jonathan281003 <98996928+Jonathan281003@users.noreply.github.com>
2023-02-28 09:50:53 -0600

    avance 28/02/2023

---
+++ CapaEntidad/CEntEmpleado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad
+{
+    public class CEntEmpleado
+    {
+        public int IDEmpleado { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public System.DateTime FechaDeNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get;set; }
+        public string Genero { get; set; }
+        public string Imagen { get; set; }
+        public Nullable<bool> Estado { get; set; }
+    }
+}
---
+++ ClaseDatos/CDatEmpleado.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidad;
+
+namespace ClaseDatos
+{
+    public class CDatEmpleado
+    {
+        //Instancia BD
+        BDLago_01Entities context = new BDLago_01Entities();
+
+        #region C >> Create >> Insertar
+        public bool GuardarNuevoEmpleado(CEntEmpleado nuevo)
+        {
+            //Instancia tbl
+            tblEmpleado Em = new tblEmpleado();
+
+            Em.IDEmpleado = nuevo.IDEmpleado;
+            Em.PrimerNombre = nuevo.PrimerNombre;
+            Em.SegundoNombre = nuevo.SegundoNombre;
+            Em.PrimerApellido = nuevo.PrimerApellido;
+            Em.SegundoApellido = nuevo.SegundoApellido;
+            Em.FechaNacimiento = nuevo.FechaDeNacimiento;
+            Em.Direccion = nuevo.Direccion;
+            Em.Telefono = nuevo.Telefono;
+            Em.Correo = nuevo.Correo;
+            Em.Genero = nuevo.Genero;
+            Em.Imagen = nuevo.Imagen;
+            Em.Estado = nuevo.Estado;
+
+            context.tblEmpleado.Add(Em);
+            context.SaveChanges();
+
+            return true;
+        }
+        #endregion
+
+        #region R >> Read >> Selecionar
+
+        #endregion
+
+        #region U >> Update >> Actualizar
+        #endregion
+
+        #region D >> Delete >> Eliminar
+        #endregion
+    }
+}
